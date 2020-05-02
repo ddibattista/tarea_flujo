4bee539f3fcef2aa61cb4d71628c962fdd6a4933
ddibattista <ddibattista@uc.cl>
2020-05-02 17:10:25 -0400

    graficos

---
+++ Codigo_Graficos.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+dias=[1,2,3,4,5,6,7]
+numero__casos__actuales=[1,4,7,12,13,6,3]
+numero__casos__nuevos=[1,3,3,7,6,4,2]
+numero__recuperados__nuevos=[0,0,0,2,5,11,5]
+
+
+cantidad_casos_actuales=[]
+cantidad_casos_totales=[]
+cantidad_casos_recuperados=[]
+casos_acumulados=0
+casos_recuperados_acumulados=0
+for dia in dias:                                                  #dias seria como la lista de los dias con lo que pasaba en cada uno
+    cantidad_casos_actuales.append(numero__casos__actuales[dia-1])       #esto es para tener una lista con los casos actuales de cada dia
+    casos_acumulados+= numero__casos__nuevos[dia-1]          #esto es para ir teniendo los casos acumulados cada dia
+    casos_recuperados_acumulados+= numero__recuperados__nuevos[dia-1]      #esto es para tener los recuperados 
+    cantidad_casos_totales.append(casos_acumulados)
+    cantidad_casos_recuperados.append(casos_recuperados_acumulados)
+
+
+def graficar_infectados(cantidad_casos_totales, cantidad_casos_recuperados, cantidad_casos_actuales, t):
+    x=np.arange(0.,int(t),1.)
+    plt.plot(x, cantidad_casos_actuales,label= 'casos actuales')
+    plt.plot(x, cantidad_casos_recuperados, label= 'casos recuperados')
+    plt.plot(x, cantidad_casos_totales, label= 'casos totales')
+    plt.ylabel('Cantidad de infectados')
+    plt.xlabel('Días')
+    plt.title("Infectados en función del tiempo")
+    
+    plt.legend(loc=0)
+    plt.savefig("grafico.png")
+    mostrar=plt.show()
+    return mostrar
+
+graficar_infectados(cantidad_casos_totales, cantidad_casos_recuperados, cantidad_casos_actuales, 7)
